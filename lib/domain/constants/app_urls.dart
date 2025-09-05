7397f827c5b2b4c40c9cf450df4b9cd2aee39ab0
sujantechx <sujantechx@gmail.com>
2025-09-05 22:00:14 +0530

    app setup ui

--- lib/domain/constants/app_urls.dart
+++ lib/domain/constants/app_urls.dart
@@ -2,6 +2,4 @@ class AppUrls {
   static const String baseUrl="https://ecommerceapi.projectnest.online/ecommerce-api/";
   static const String loginUrl="$baseUrl/user/login";
   static const String registerUrl="$baseUrl/user/registration";
-
-
 }
